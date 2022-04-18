30e81d17d6682825c3807d8ef802ae7756013b08
irtgal <gal.irt.01@gmail.com>
2022-04-18 23:38:32 +0200

    dodan rezervirano timestamp v mail

--- resources/views/emails/new_reservation.blade.php
+++ resources/views/emails/new_reservation.blade.php
@@ -3,3 +3,6 @@
 <p>Rezerviral: <strong>{{ $name }}</strong></p>
 <p>Storitev: <strong>{{ $type_name }}</strong></p>
 <p>Kontakt: <strong>{{ $contact }}</strong></p>
+<br>
+<br>
+<small>Rezervirano ob {{date('h:i d.m.Y')}}</small>
